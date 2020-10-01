3e5d5aafdc5249c435a185174fa2ca228a4dab69
Amy M Haddad <amy.m.haddad@gmail.com>
2020-10-01 07:28:35 -0400

    Built out component structure

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import SignIn from "./components/SignIn"
+import SignUp from "./components/SignUp"
+import Users from "./components/Users"
+import Header from "./common/Header"
+ 
+function App() {
+  return (
+    <div>
+        <Header />`≠
+        <Route to="/signup" exact component={SignUp} />
+        <Route to="/signin" exact component={SignIn} />
+        <Route to="/users" exact component={Users} />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/common/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+
+
+function Header() {
+    const activeStyle = {color: "navy"}
+
+    return (
+        <>
+        <NavLink activeStyle={activeStyle} to="/signup">Sign Up</NavLink>
+        {' | '}
+        <NavLink activeStyle={activeStyle} to="/signin">Sign In</NavLink>
+        {' | '}
+        <NavLink activeStyle={activeStyle} to="/users">See Users</NavLink>
+        </>
+    )
+}
+
+export default Header;
