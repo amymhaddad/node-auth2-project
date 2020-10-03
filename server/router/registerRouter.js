52e4d3d4f0d6ea93bdc3ecaffd3288520edc2e4b
Amy M Haddad <amy.m.haddad@gmail.com>
2020-10-03 10:57:37 -0400

    Added token at register

--- server/router/registerRouter.js
+++ server/router/registerRouter.js
@@ -4,6 +4,14 @@ const CryptoJS = require('crypto-js');
 
 const { verifyNewUserCredentials, handleErrors } = require('../middlewares/Middleware');
 const User = require('../data/usersModel');
+const jwt = require('jsonwebtoken');
+
+function newToken(id) {
+	const userId = { userId: id}
+	return jwt.sign(userId, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1800s'})
+
+}
+
 
 registerRouter.post('/', [ verifyNewUserCredentials, handleErrors ], (req, res, next) => {
 	const { username, password, department } = req.body;
@@ -19,9 +27,13 @@ registerRouter.post('/', [ verifyNewUserCredentials, handleErrors ], (req, res, 
 	User.addUser(newUserObject)
 		.then((newUser) => {
 			const addedUser = { username: newUser.username, department: newUser.department };
+
+			const token = newToken(newUser.id)
+			// return res.status(200).json({ token });			// 
 			return res.status(200).json(addedUser);
 		})
 		.catch((err) => next(err));
+	 
 });
 
 module.exports = registerRouter;
