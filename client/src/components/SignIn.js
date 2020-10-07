4d1194bf994163657a62ff63b535e3200e87d3ab
Amy M Haddad <amy.m.haddad@gmail.com>
2020-10-07 07:00:57 -0400

    Refactored error handling funciton in signin

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -6,7 +6,6 @@ import SignInForm from "./SignInForm"
 import { useHistory } from "react-router-dom";
 import { toast } from "react-toastify"
 
-
 function SignIn() {
     let history = useHistory();
     const [errors, setErrors] = useState({})
@@ -33,16 +32,23 @@ function SignIn() {
         toast.success("Success!")
         history.push({
             pathname: '/users',
-            //check if I can remove the userID
             state: { userId: token.split(".")[1]}
           })
     }
 
-
-    // Pass in usercred into postApiLogin
+    function handleUnsuccessfulLogin(error) {
+        const message  = error.response.data.error
+        const status = error.response.stats
+        const userError = {
+            message: message, 
+            status: status
+        }
+            setErrors(userError)
+            clearForm()
+    }
 
     //Extracted the axios call -- fewer dependencies so it can go in its own file 
-    //Created a sep function to handle the response. I did this in the code bc otherwise I'd have to pass in: toast, history, response
+    //Created a separate function here to handle the response. I did this in the code bc otherwise I'd have to pass in: toast, history, response
     function handleSubmit(event) {
         event.preventDefault()
         postApiLogin(userCredentials)
@@ -52,17 +58,21 @@ function SignIn() {
                 handleSuccussfulLogin(token)
             }
         })
-
-        //extract this as its own function 
         .catch(function(error) {
-            const message  = error.response.data.error
-            const status = error.response.stats
-            const userError = {
-                message: message, 
-                status: status
-            }
-            setErrors(userError) 
-            clearForm()
+           handleUnsuccessfulLogin(error)
+            // setErrors(userError)
+            // clearForm()
+        //extract this as its own function 
+        // function handleErrors(errors)
+        // .catch(function(error) {
+            // const message  = error.response.data.error
+            // const status = error.response.stats
+            // const userError = {
+            //     message: message, 
+            //     status: status
+            // }
+            // setErrors(userError) 
+            // clearForm()
         })
     }
 
@@ -80,7 +90,6 @@ function SignIn() {
                 <div>
                     {errors.message} {"  "}
                     {errors.status}
-                    
                 </div>
             )}
 
